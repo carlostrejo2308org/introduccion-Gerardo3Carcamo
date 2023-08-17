7170986fd7c609604d5f17473c064977997054c6
Gerardo3Carcamo <127584195+Gerardo3Carcamo@users.noreply.github.com>
2023-08-17 00:02:42 -0600

    Feat 2023/08/17
    -Fibonacci function created
    -Collaborators file text created

--- Simulacion/src/Main/Fibonacci.java
+++ Simulacion/src/Main/Fibonacci.java
@@ -4,14 +4,25 @@
  */
 package Main;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author 000093883
  */
 public class Fibonacci {
     
+    public static int fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+    
     public static void main(String[] args) {
-        
+        int number = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el numero de fibonacci a ver"));
+        int fibonacciNumber = fibonacci(number);
+        JOptionPane.showMessageDialog(null, String.valueOf(fibonacciNumber));
     }
     
 }
