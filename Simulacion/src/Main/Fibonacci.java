a7ff0a5b79b62c993dd1b97bb21daba6b508ffbf
Gerardo3Carcamo <127584195+Gerardo3Carcamo@users.noreply.github.com>
2023-08-16 23:56:51 -0600

    Feat 16/08/2023
    Project Simulacion created

---
+++ Simulacion/src/Main/Fibonacci.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Main;
+
+/**
+ *
+ * @author 000093883
+ */
+public class Fibonacci {
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
